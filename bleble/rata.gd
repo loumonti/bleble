a43338d48335229f98e36c25708ffca8661649bb
tats morales <77303353+itsbeesfan@users.noreply.github.com>
2026-03-01 13:13:21 -0500

    levels and ui and stuff

--- bleble/rata.gd
+++ bleble/rata.gd
@@ -27,5 +27,8 @@ func _physics_process(delta: float) -> void:
 	var isLeft = velocity.x > 0
 	rata.flip_h = isLeft
 
+
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://game_over.tscn")
