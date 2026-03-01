c7448d7917a0181b8cc7eed1becb7c52ded483ca
tats morales <77303353+itsbeesfan@users.noreply.github.com>
2026-03-01 12:21:20 -0500

    nivel 1 y rata

--- bleble/rata.gd
+++ bleble/rata.gd
@@ -7,15 +7,17 @@ var health =- 1
 
 func add_gravity(delta):
 	if not is_on_floor():
-		velocity.y += get_gravity() * delta
+		velocity += get_gravity() * delta
 		
 func move_rat():
 	velocity.x = speed * direction
 	
-func reverse_drection():
+func reverse_direction():
 	if is_on_wall():
 		direction =- direction
-		
+		 
 func _physics_process(delta: float) -> void:
 	add_gravity(delta)
 	move_rat()
+	move_and_slide()
+	reverse_direction()
