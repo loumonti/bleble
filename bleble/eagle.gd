1e5eb3f0e7e96250f8c44d0ac2bfe213ce746dc3
tats morales <77303353+itsbeesfan@users.noreply.github.com>
2026-03-01 09:43:58 -0500

    added eagle following the topo

---
+++ bleble/eagle.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export var speed: float = 100
+@export var topo: Node2D
+
+func _physics_process(delta: float) -> void:
+	if topo == null:
+		return
+		
+	var direction = (topo.global_position - global_position).normalized()
+	velocity = velocity.lerp(direction * speed, 0.1)
+	
+	move_and_slide()
