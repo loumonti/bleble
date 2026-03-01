0a7a15e84b3a915f3601fb4c5ba5183807e2d2eb
tats morales <77303353+itsbeesfan@users.noreply.github.com>
2026-03-01 12:12:26 -0500

    rata

--- bleble/rata.gd
+++ bleble/rata.gd
@@ -1,1 +1,21 @@
-extends Area2D
+extends CharacterBody2D
+
+const speed = 70.0
+
+var direction = 1
+var health =- 1
+
+func add_gravity(delta):
+	if not is_on_floor():
+		velocity.y += get_gravity() * delta
+		
+func move_rat():
+	velocity.x = speed * direction
+	
+func reverse_drection():
+	if is_on_wall():
+		direction =- direction
+		
+func _physics_process(delta: float) -> void:
+	add_gravity(delta)
+	move_rat()
