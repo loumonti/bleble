6d9970907949d3f66eb67370d66c0d51a6d3b04b
tats morales <77303353+itsbeesfan@users.noreply.github.com>
2026-04-02 23:36:05 -0500

    level switching fixed!

--- bleble/scenes/level_3_button.gd
+++ bleble/scenes/level_3_button.gd
@@ -1,19 +1,12 @@
-extends Control
+extends Button
 
+@export var level_number := 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
-
-
-func _on_button_2_pressed() -> void:
-	get_tree().quit()
--- bleble/scripts/level_select.gd
+++ bleble/scripts/level_select.gd
@@ -23,6 +23,9 @@ func _on_level_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level-1.tscn")
 	
 
-
 func _on_level_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level-2.tscn")
+
+
+func _on_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level-3.tscn")
