d77a0b84213c16767722b1cc073170c095da36ce
tats morales <77303353+itsbeesfan@users.noreply.github.com>
2026-04-07 01:55:55 -0500

    change fonts

--- bleble/scripts/ganaste.gd
+++ bleble/scripts/ganaste.gd
@@ -11,9 +11,11 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
-
-
-func _on_texture_button_pressed() -> void:
+func _on_ragequit_pressed() -> void:
 	get_tree().quit()
+	
+
+
+
+func _on_next_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
